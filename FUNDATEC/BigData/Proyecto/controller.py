c15165bd3b59f8e449c6e740e914c0a470abe908
Marco Ferraro <mantofer2000@gmail.com>
2023-12-27 20:52:17 -0600

    changing read

--- FUNDATEC/BigData/Proyecto/controller.py
+++ FUNDATEC/BigData/Proyecto/controller.py
@@ -1,14 +1,22 @@
 import functions
+import pprint
 
 
-def read_csv_data(file_paths, spark):
-    df = spark.read.csv(file_paths, header=True, inferSchema=True)
+def read_csv_data(file_path, spark):
+    df = spark.read.csv(file_path, header=True, inferSchema=True)
 
     return df
 
 
 def run(file_paths):
     spark, sc = functions.init_spark()
+    pp = pprint.PrettyPrinter(indent=4)
 
-    df = read_csv_data(file_paths, spark)
-    print(df.show())
+    database = {}
+    for file_path in file_paths:
+        db_name = file_path.split('/')[-1].split('.')[0]
+        print(f"Reading data from {db_name} database...")
+
+        # database[db_name] = read_csv_data(file_path, spark)
+
+    pp.pprint(database)
