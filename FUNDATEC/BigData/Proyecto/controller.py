71417fd8e1f11fd3b21c14155b5afef944f42f91
Marco Ferraro <mantofer2000@gmail.com>
2024-01-05 20:55:55 -0600

    proposition of join

--- FUNDATEC/BigData/Proyecto/controller.py
+++ FUNDATEC/BigData/Proyecto/controller.py
@@ -17,6 +17,6 @@ def run(file_paths):
         db_name = file_path.split('/')[-1].split('.')[0]
         print(f"Reading data from {db_name} database...")
 
-        # database[db_name] = read_csv_data(file_path, spark)
+        database[db_name] = read_csv_data(file_path, spark)
 
-    pp.pprint(database)
+    pp.pprint(database["climate_data_nepal"].show())
--- FUNDATEC/BigData/Proyecto/functions.py
+++ FUNDATEC/BigData/Proyecto/functions.py
@@ -1,3 +1,4 @@
+from pyspark.sql.functions import to_date
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.functions import explode
@@ -8,3 +9,28 @@ def init_spark():
     spark = SparkSession.builder.appName("HomeWork_MarcoFerraro").getOrCreate()
     sc = spark.sparkContext
     return spark, sc
+
+
+# Initialize SparkSession
+# spark = SparkSession.builder.appName("DateTransformAndJoin").getOrCreate()
+
+# Sample data for DataFrame df1 with date format '2013-06-16'
+# data1 = [("A", "2013-06-16"), ("B", "2013-06-17")]
+# df1 = spark.createDataFrame(data1, ["Key", "Date1"])
+
+# Sample data for DataFrame df2 with date format '7/30/2016'
+# data2 = [("A", "7/30/2016"), ("C", "7/31/2016")]
+# df2 = spark.createDataFrame(data2, ["Key", "Date2"])
+
+# Transform date formats to 'yyyy-MM-dd' for both DataFrames
+# df1 = df1.withColumn("Date1", to_date(df1["Date1"]))
+# df2 = df2.withColumn("Date2", to_date(df2["Date2"], "M/d/yyyy"))
+
+# Perform join operation on the common key column and date column
+# joined_df = df1.join(df2, on=["Key"]).filter(df1["Date1"] == df2["Date2"])
+
+# Show the joined DataFrame
+# joined_df.show()
+
+# Stop the SparkSession
+# spark.stop()
