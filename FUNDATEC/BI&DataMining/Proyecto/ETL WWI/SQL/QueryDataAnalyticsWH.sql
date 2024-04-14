273608385145c3f976d3d6da219bfe67d158ee44
mantofer02 <mantofer2000@gmail.com>
2024-04-14 17:45:22 -0600

    big dog

---
+++ FUNDATEC/BI&DataMining/Proyecto/ETL WWI/SQL/QueryDataAnalyticsWH.sql
@@ -0,0 +1,6 @@
+DROP TABLE AnalyticsBU.Sales.OrdersByCity;
+
+
+SELECT * FROM AnalyticsBU.Sales.OrdersByDelivery;
+SELECT * FROM AnalyticsBU.Sales.OrdersByCity;
+SELECT * FROM AnalyticsBU.Sales.OrdersByCustomer;
